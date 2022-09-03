179faa83dc1ac3cf98745154267ef1833fa2b19d
Sebastian Paarmann <git@s-paarmann.de>
2022-09-03 17:08:24 +0200

    Move upload functionality into its own module

---
+++ src/upload.rs
@@ -0,0 +1,140 @@
+use std::{io::ErrorKind, net::SocketAddr, path::PathBuf, sync::Arc};
+
+use axum::{
+    body::Bytes,
+    extract::{ConnectInfo, Multipart},
+    http::StatusCode,
+    Extension, Router,
+};
+use miette::{miette, Context, IntoDiagnostic};
+use tokio::fs::OpenOptions;
+use tracing::Instrument;
+
+#[derive(knuffel::Decode, Debug)]
+pub struct Config {
+    #[knuffel(child, unwrap(argument))]
+    route: String,
+    #[knuffel(child, unwrap(argument))]
+    target_dir: PathBuf,
+    #[knuffel(child, unwrap(argument))]
+    filename_length: usize,
+}
+
+pub fn setup(config: Config, router: Router) -> miette::Result<Router> {
+    let config = Arc::new(config);
+
+    let upload_target_meta = std::fs::metadata(&config.target_dir)
+        .into_diagnostic()
+        .wrap_err("Failed to check metadata of upload target dir")?;
+
+    if !upload_target_meta.is_dir() {
+        return Err(miette!(
+            "Upload target path {} is not a directory!",
+            config.target_dir.display()
+        ));
+    }
+
+    Ok(router
+        .route(&config.route, axum::routing::get(get))
+        .route(&config.route, axum::routing::post(post))
+        .layer(Extension(config)))
+}
+
+#[tracing::instrument]
+async fn get(ConnectInfo(client_addr): ConnectInfo<SocketAddr>) -> &'static str {
+    tracing::info!("GET upload");
+    "POST to this address to upload files"
+}
+
+#[tracing::instrument(skip(body, upload_config))]
+async fn post(
+    body: Multipart,
+    ConnectInfo(client_addr): ConnectInfo<SocketAddr>,
+    Extension(upload_config): Extension<Arc<Config>>,
+) -> Result<String, StatusCode> {
+    tracing::info!("Upload request");
+
+    let (original_name, bytes) = get_file_name_and_bytes(body).await?;
+
+    // We want to preserve the original file extension, while replacing the rest of the file name
+    // with a random short name.
+    let extension = original_name
+        .rsplit_once('.')
+        .ok_or(StatusCode::BAD_REQUEST)?
+        .1;
+
+    loop {
+        let mut name = generate_name(upload_config.filename_length);
+        name.push('.');
+        name.push_str(&extension);
+
+        let mut path = upload_config.target_dir.clone();
+        path.push(&name);
+
+        let mut file = match OpenOptions::new()
+            .write(true)
+            .create_new(true)
+            .open(&path)
+            .await
+        {
+            // happened to get a random path that already exists, try again
+            Err(e) if e.kind() == ErrorKind::AlreadyExists => continue,
+            Err(e) => {
+                tracing::error!(path = ?path, error = ?e, "Error opening file for upload");
+                return Err(StatusCode::INTERNAL_SERVER_ERROR);
+            }
+            Ok(f) => f,
+        };
+
+        tokio::io::copy_buf(&mut bytes.as_ref(), &mut file)
+            .instrument(tracing::info_span!("Writing file", path = ?path))
+            .await
+            .map_err(|e| {
+                tracing::error!(error = ?e, "Error writing file");
+                StatusCode::INTERNAL_SERVER_ERROR
+            })?;
+
+        tracing::info!(path = ?path, "Uploaded file");
+
+        return Ok(name);
+    }
+}
+
+async fn get_file_name_and_bytes(mut body: Multipart) -> Result<(String, Bytes), StatusCode> {
+    let field = body
+        .next_field()
+        .await
+        .map_err(|_| StatusCode::BAD_REQUEST)?
+        .ok_or(StatusCode::BAD_REQUEST)?;
+
+    let field_name = field.name();
+    if field_name != Some("file") {
+        return Err(StatusCode::BAD_REQUEST);
+    }
+
+    let file_name = field
+        .file_name()
+        .ok_or(StatusCode::BAD_REQUEST)?
+        .to_string();
+    let bytes = field.bytes().await.map_err(|_| StatusCode::BAD_REQUEST)?;
+
+    tracing::info!("Got file {} with {} bytes", file_name, bytes.len());
+    Ok((file_name, bytes))
+}
+
+fn generate_name(len: usize) -> String {
+    fn num_to_char(num: usize) -> char {
+        match num {
+            0..=25 => (b'a' + num as u8) as char,
+            26..=51 => (b'A' + (num - 26) as u8) as char,
+            52..=61 => char::from_digit((num - 52).try_into().unwrap(), 10).unwrap(),
+            _ => panic!("invalid num for converting to char!"),
+        }
+    }
+
+    use rand::prelude::*;
+    let mut rng = thread_rng();
+    (0..len)
+        .map(|_| num_to_char(rng.gen_range(0..=61)))
+        .collect()
+}
